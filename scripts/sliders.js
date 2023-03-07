58037377d0eec6d4623f946b6849a5febeafb756
alex genetica <duy.pham@genefriendway.com>
2023-03-07 19:49:46 +0700

    Improve dev env

--- scripts/sliders.js
+++ scripts/sliders.js
@@ -1,1 +1,14 @@
-console.log('Slider')
+jQuery(window).load(function () {
+  new Swiper('.swiper', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+    }
+  })
+});
